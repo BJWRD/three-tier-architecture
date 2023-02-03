f88b4f0cb3606f07e26359befcac970249a63110
BJWRD <brad.whitcomb@sky.com>
2023-02-03 21:20:01 +0000

    S3 Bucket Inclusion and Versioning Updates

--- versions.tf
+++ versions.tf
@@ -1,10 +1,16 @@
 terraform {
-  required_version = "~> 0.15.0"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.44.0"
+      version = ">= 4.50.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "tfstate-dev-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
   }
 }
