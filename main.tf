32356bd87a0a613562a9eed5481a02794d6edb2d
BJWRD <brad.whitcomb@sky.com>
2023-02-04 11:46:51 +0000

    Initial Commit

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = var.region
 }
 
 module "ec2" {
   source                 = "./modules/ec2"
+  project_name           = var.project_name
+  environment            = var.environment
   image_id               = var.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = module.security.vpc_security_group_ids
   id_app                 = var.id_app
   name_prefix            = var.name_prefix
-  project_name           = var.project_name
-  environment            = var.environment
 }
 
 module "vpc" {
   source                      = "./modules/vpc"
+  project_name                = var.project_name
+  environment                 = var.environment
   vpc_id                      = var.vpc_id
   vpc_cidr                    = var.vpc_cidr
   availability_zones          = var.availability_zones
@@ -27,37 +29,27 @@ module "vpc" {
   id_app                      = module.ec2.id_app
   security_group_alb_app_http = module.security.security_group_alb_app_http
   subnet_id                   = var.subnet_id
-  az_public_subnet            = var.az_public_subnet
-  az_private_subnet           = var.az_private_subnet
-  az_database_subnet          = var.az_database_subnet
+  az_public_subnet            = var.public_subnet
+  az_private_subnet           = var.private_subnet
+  az_database_subnet          = var.database_subnet
   app_alb_lb                  = var.app_alb_lb
   app_target_group            = var.app_target_group
   app_autoscaling_group       = var.app_autoscaling_group
   database_subnet_group       = var.database_subnet_group
-  project_name                = var.project_name
-  environment                 = var.environment
-}
-
-module "security" {
-  source                      = "./modules/security"
-  vpc_id                      = module.vpc.vpc_id
   alb_app_security_group      = var.alb_app_security_group
   app_instance_security_group = var.app_instance_security_group
   db_security_group           = var.db_security_group
-  project_name                = var.project_name
-  environment                 = var.environment
   cidr_blocks                 = var.cidr_blocks
 }
 
 module "rds" {
   source                               = "./modules/rds"
-  aws_db_subnet_group_db_subnet        = module.vpc.aws_db_subnet_group_db_subnet
-  aws_security_group_db_security_group = module.security.aws_security_group_db_security_group
   project_name                         = var.project_name
   environment                          = var.environment
   engine_name                          = var.engine_name
   engine_version                       = var.engine_version
   storage                              = var.storage
+  aws_db_subnet_group_db_subnet        = module.vpc.aws_db_subnet_group_db_subnet_group
   identifier                           = var.identifier
   instance_class                       = var.instance_class
   multi_az                             = var.multi_az
@@ -66,5 +58,6 @@ module "rds" {
   database_password                    = var.database_password
   database_port                        = var.database_port
   publicly_accessible                  = var.publicly_accessible
+  aws_security_group_db_security_group = module.security.aws_security_group_db_sg
   database_snapshot                    = var.database_snapshot
 }
