efe90e43d6bf8fb57321b6ff1e9ff917b90ae1fd
BJWRD <brad.whitcomb@sky.com>
2023-02-07 14:01:31 +0000

    Resource Rename

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,11 +6,11 @@ locals {
 }
 
 # App - Launch Template
-resource "aws_launch_template" "app_launch_template" {
+resource "aws_launch_template" "main" {
   name_prefix            = var.name_prefix
   image_id               = var.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.vpc_security_group_ids]
+  vpc_security_group_ids = var.vpc_security_group_ids
   user_data              = filebase64("./modules/ec2/install.sh")
 
   tags = local.required_tags
