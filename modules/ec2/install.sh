4d43b662de84a3bd24c1fb22455ba2e79b07c85d
BJWRD <brad.whitcomb@sky.com>
2023-02-07 14:00:28 +0000

    Bash Script Update

--- modules/ec2/install.sh
+++ modules/ec2/install.sh
@@ -6,3 +6,4 @@ sudo systemctl enable docker --now
 sudo systemctl enable containerd.service --now
 sudo usermod -a -G docker ec2-user
 sudo docker pull bjwrd/app.py
+sudo docker run -d bjwrd/app.py 
