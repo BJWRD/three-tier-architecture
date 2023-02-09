af49f350cc07aea2d3a43ec13638e6a5efd8f0ad
BJWRD <brad.whitcomb@sky.com>
2023-02-09 19:56:02 +0000

    Variable Name Update

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -18,8 +18,8 @@ variable "instance_type" {
   type = string
 }
 
-variable "vpc_security_group_ids" {
-  description = "A list of security group IDs to associate with"
+variable "app_security_group" {
+  description = "Application Security Group"
   type = string
 }
 
