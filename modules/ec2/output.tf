c436573e879aa8037b5e5c2b3fbe9d08497e5367
BJWRD <brad.whitcomb@sky.com>
2023-02-09 19:55:23 +0000

    instance_ip_addr Removal

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -2,7 +2,3 @@ output "id_app" {
   description = "Launch Template ID"
   value = aws_launch_template.main.id
 }
-
-output "instance_ip_addr"
-  description = "The private IP address of the main server instance."
-  value = aws_launch_template.public_ip
