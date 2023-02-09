5f774a671b8773ee9f035cb00809f6b8edc74d57
BJWRD <brad.whitcomb@sky.com>
2023-02-09 19:54:42 +0000

    Variable Update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -10,7 +10,7 @@ resource "aws_launch_template" "main" {
   name_prefix            = var.name_prefix
   image_id               = var.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.app_security_group]
   user_data              = filebase64("./modules/ec2/install.sh")
 
   tags = local.required_tags
